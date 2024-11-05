5d210223fc4659e40b71210f8f153b87f8ec4e4f
Yip, Kwan Yu <133111+223136328@users.noreply.github.build.ge.com>
2024-11-05 16:09:19 +0000

    Initial Commit

---
+++ ScrumdingerApp/ScrumdingerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerAppApp.swift
+//  ScrumdingerApp
+//
+//  Created by Kyan Yip on 05/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

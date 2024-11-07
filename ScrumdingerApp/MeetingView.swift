f1f3786a1d1e745eaaa834e60030109a5f2dcabb
Yip, Kwan Yu <133111+223136328@users.noreply.github.build.ge.com>
2024-11-07 11:58:00 +0000

    Initial Meeting View

---
+++ ScrumdingerApp/MeetingView.swift
@@ -0,0 +1,46 @@
+//
+//  ContentView.swift
+//  ScrumdingerApp
+//
+//  Created by Kyan Yip on 05/11/2024.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            ProgressView(value: 10, total:15)
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Seconds Elapsed")
+                        .font(.caption)
+                    Label("300", systemImage: "hourglass.tophalf.fill")
+                    // title 300, image
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Seconds Remaining")
+                        .font(.caption)
+                    Label("600", systemImage: "hourglass.bottomhalf.fill")
+                }
+            }
+            Circle()
+                .strokeBorder(lineWidth: 24)
+            HStack {
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button(action: {}) {
+                    Image(systemName: "forward.fill")
+                }
+            }
+        }
+        .padding()
+    }
+}
+
+struct MeetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView()
+    }
+}
